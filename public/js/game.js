32ed8009d13f85ddba9760588fd85ecf3d34b23a
unknown <guillory1985@gmail.comgit config --global user.name Terry>
2020-11-08 20:04:05 -0600

    fix sound

--- public/js/game.js
+++ public/js/game.js
@@ -2,12 +2,10 @@
 
 for (let i = 0; i < document.querySelectorAll(".letter").length; i++) {
     document.querySelectorAll(".letter")[i].addEventListener("click", function(){ //listened to for the div to be click
-        // let sounds = new Audio('sounds/snare.mp3'); // sound that will happened once the div is clicked
-        // sounds.play();
 
         let spanText = document.querySelectorAll("span")[i].innerText;
 
-        let sounds = new Audio('sounds/Letter ' + spanText + '.m4a'); // sound that will happened once the div is clicked
+        let sounds = new Audio('sounds/letter ' + spanText + '.m4a'); // sound that will happened once the div is clicked
         sounds.play();
         
         console.log(spanText);
